b162611493af150b6a5ef8e8bc29d6504de2efc9
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-08-05 02:53:55 +0530

    Update Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
+import { BiChevronLeftSquare, BiChevronRightSquare } from "react-icons/bi";
 
-const Sidebar = () => {
-	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput />
-			<div className='divider px-3'></div>
-			<Conversations />
-			<LogoutButton />
-		</div>
-	);
+const Sidebar = ({ isCollapsed, toggleSidebar }) => {
+  return (
+    <div
+      className={`border-r border-slate-500 p-4 flex flex-col ${
+        isCollapsed ? "w-[15%]" : "w-[50%]"
+      }`}
+    >
+      {!isCollapsed && (
+        <>
+          <SearchInput />
+          <div className="divider px-3"></div>
+          <Conversations />
+        </>
+      )}
+      <div className="mt-auto flex justify-end">
+        <button onClick={toggleSidebar}>
+          {isCollapsed ? (
+            <BiChevronRightSquare className="w-6 h-6 text-white cursor-pointer" />
+          ) : (
+            <BiChevronLeftSquare className="w-6 h-6 text-white cursor-pointer" />
+          )}
+        </button>
+      </div>
+      <LogoutButton />
+    </div>
+  );
 };
+
 export default Sidebar;
 
 // STARTER CODE FOR THIS FILE
