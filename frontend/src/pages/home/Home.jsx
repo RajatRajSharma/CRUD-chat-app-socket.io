fd04e2eff9b9ad9edba5464db65d524d82385787
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-08-05 02:54:32 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
-	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
-		</div>
-	);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  return (
+    <div
+      className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0"
+      style={{ maxHeight: "70%" }}
+    >
+      <Sidebar isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} />
+      <div
+        className={`flex flex-col ${
+          isCollapsed ? "w-[85%]" : "w-[50%]"
+        } md:min-w-[450px] `}
+        style={{ minHeight: "550px" }}
+      >
+        <MessageContainer />
+      </div>
+    </div>
+  );
 };
+
 export default Home;
