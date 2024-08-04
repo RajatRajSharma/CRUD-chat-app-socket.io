55bdec5c7a895e1dc20ec1d757218f543028dd2e
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-08-05 02:52:24 +0530

    Update MessageContainer.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -38,7 +38,10 @@ export default MessageContainer;
 const NoChatSelected = () => {
   const { authUser } = useAuthContext();
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div
+      className="flex items-center justify-center w-full"
+      style={{ height: "100%" }}
+    >
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
         <p>Welcome 👋 {authUser.fullName} ❄</p>
         <p>Select a chat to start messaging</p>
